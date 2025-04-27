9baf743d1078eb806a427159c5001fd08e020900
MohammadSiam <siamk417@gmail.com>
2025-04-27 09:15:59 +0600

    fixed build error

--- src/pages/project/ProjectCard.tsx
+++ src/pages/project/ProjectCard.tsx
@@ -18,17 +18,17 @@ const ProjectCard = ({ project }: { project: IProject }) => {
       <div className="relative md:px-20 md:p-0 rounded-2xl transform duration-500 hover:shadow-2xl hover:-translate-y-1">
         <Image
           className="h-[412] object-cover py-2 rounded-2xl"
-          src={project.imageURL}
+          src={project?.imageURL}
           width={733}
           height={412}
-          alt={project.title}
+          alt={project?.title}
         />
         <div className="content bg-white p-2 pt-8 md:p-12 pb-12 lg:max-w-lg w-full lg:absolute top-20 right-5">
           <div className="flex justify-between font-bold text-sm">
             <p>Project Review</p>
-            <p className="text-gray-400">{project.completionTime}</p>
+            <p className="text-gray-400">{project?.completionTime}</p>
           </div>
-          <h2 className="font-semibold mt-2 md:mt-10">{project.title}</h2>
+          <h2 className="font-semibold mt-2 md:mt-10">{project?.title}</h2>
           <button
             onClick={openModal}
             className="mt-2 md:mt-5 p-3 px-5 bg-black text-white font-bold text-sm cursor-pointer"

8cad159008ba6dcef3998789b6080b53555392d1
MohammadSiam <siamk417@gmail.com>
2025-04-30 07:18:49 +0600

    fixed image uploader in blog create page

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -1,12 +1,47 @@
 "use client";
 import dynamic from "next/dynamic";
-import { useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import { FiUpload } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 const NoSSR = dynamic(() => import("@/pages/blog/RichTextEditor"), {
   ssr: false,
 });
 
+interface BlogFormData {
+  title: string;
+  description: string;
+  blogTags: string;
+  blogImageURL: File | null;
+}
+
 function CreateBlog() {
   const editorRef = useRef<any>(null);
+  const [previewImage, setPreviewImage] = useState<string>("");
+  const [formData, setFormData] = useState<BlogFormData>({
+    title: "",
+    description: "",
+    blogTags: "",
+    blogImageURL: null,
+  });
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setFormData((prev) => ({ ...prev, imageFile: file }));
+      const imageUrl = URL.createObjectURL(file);
+      setPreviewImage(imageUrl);
+    }
+  };
+
+  const handleImageUploadClick = () => {
+    document.getElementById("image_input")?.click();
+  };
+
+  const handleRemoveImage = () => {
+    setFormData((prev) => ({ ...prev, imageFile: null }));
+    setPreviewImage("");
+  };
+
   const handleSubmit = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
@@ -19,6 +54,38 @@ function CreateBlog() {
         <div className="container">
           <div className="text-center mb-5">
             <h2 className="text-3xl font-bold">Create New Blog</h2>
+          </div>
+          <div className="flex flex-col items-center gap-5">
+            <input
+              type="file"
+              id="image_input"
+              name="imageURL"
+              className="hidden"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+
+            {previewImage === "" ? (
+              <div
+                className="w-full flex flex-col items-center justify-center gap-4 border border-dashed border-gray-300 rounded-md py-8 cursor-pointer"
+                onClick={handleImageUploadClick}
+              >
+                <FiUpload className="text-3xl text-gray-400" />
+                <p className="text-gray-400">Browse to upload your image</p>
+              </div>
+            ) : (
+              <div className="relative w-full h-auto">
+                <img
+                  src={previewImage}
+                  alt="Preview"
+                  className="w-full h-full object-cover rounded-md"
+                />
+                <MdDelete
+                  className="absolute top-2 right-2 text-3xl text-red-600 bg-white rounded-full p-1 cursor-pointer"
+                  onClick={handleRemoveImage}
+                />
+              </div>
+            )}
           </div>
           <NoSSR editorRef={editorRef} />
           <div className="my-3 flex justify-center">
